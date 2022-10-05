90e69955007191a3f66c5390ac918d31bb5068c0
KenzoUMZ <kenzoumezawa@gmail.com>
2022-10-05 00:05:47 -0300

    basic layout

--- lib/constants/app_fonts.dart
+++ lib/constants/app_fonts.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:this_is_me/constants/app_colors.dart';
 
 //login screen
 var loginButton = GoogleFonts.ubuntu(fontSize: 20);
-var forgotPasswordButton =
-    GoogleFonts.ubuntu(fontSize: 15, color: Colors.grey);
-var newAccountButton =
-    GoogleFonts.ubuntu(fontSize: 20, color: Colors.white);
+var forgotPasswordButton = GoogleFonts.ubuntu(fontSize: 15, color: Colors.grey);
+var newAccountButton = GoogleFonts.ubuntu(fontSize: 20, color: Colors.white);
+
+// Character screen
+var screenTitle = GoogleFonts.ubuntu(fontSize: 25, color: litePurple);
+var characterLevel = GoogleFonts.ubuntu(fontSize: 25, color: Colors.white);
+var characterName = GoogleFonts.ubuntu(fontSize: 20, color: Colors.white);
--- lib/view/character_screen.dart
+++ lib/view/character_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:this_is_me/constants/app_colors.dart';
+import 'package:this_is_me/constants/app_fonts.dart';
 import 'package:this_is_me/model/character.dart';
 
 class CharacterScreen extends StatefulWidget {
@@ -19,9 +21,48 @@ class _CharacterScreenState extends State<CharacterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-          children: [(Center(child: Text(widget.character.characterName)))]),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: background,
+      body: Padding(
+          padding: const EdgeInsets.only(top: 60),
+          child: Column(children: [
+            Center(
+                child: Text(
+              'Quadros',
+              style: screenTitle,
+            )),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  children: [leftColumn(), rightColumn()],
+                ))
+          ])),
     );
+  }
+
+  Widget leftColumn() {
+    return Column(
+      children: [
+        Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Column(children: [
+              Text(
+                'Nv.${widget.character.level}',
+                style: characterLevel,
+              ),
+              Text(
+                widget.character.characterName,
+                style: characterName,
+              ), SizedBox(
+                
+                width: 200,
+                child:Image.asset('assets/images/personagemFem1.png'))
+            ]))
+      ],
+    );
+  }
+
+  Widget rightColumn() {
+    return Column();
   }
 }

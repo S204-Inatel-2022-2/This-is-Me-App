1ebcf649f6c317926d36f526515acd41c7481be7
KenzoUMZ <kenzoumezawa@gmail.com>
2022-10-05 18:37:03 -0300

    fix: files path

--- lib/components/login_components.dart
+++ lib/components/login_components.dart
@@ -6,8 +6,9 @@ import 'package:this_is_me/constants/app_fonts.dart';
 import 'package:this_is_me/controller/user_controller.dart';
 import 'package:this_is_me/model/character.dart';
 import 'package:this_is_me/view/character_screen.dart';
-import 'package:this_is_me/view/account/registration_screen.dart';
+
 import 'package:http/http.dart' as http;
+import 'package:this_is_me/view/account/registration_screen.dart';
 
 // TextEditingController
 TextEditingController emailController = TextEditingController();
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:this_is_me/view/login_screen.dart';
+import 'package:this_is_me/view/account/login_screen.dart';
 
 void main() {
   runApp(const MaterialApp(home: LoginScreen()));

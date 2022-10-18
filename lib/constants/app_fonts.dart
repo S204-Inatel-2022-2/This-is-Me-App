39eeb94b00e37d51b9cd1f8a3ea89d802fad4101
Kenzo Umezawa <kenzoumezawa@gmail.com>
2022-10-18 15:04:22 -0300

    objeto quest

--- lib/constants/app_fonts.dart
+++ lib/constants/app_fonts.dart
@@ -11,21 +11,32 @@ var newAccountButton = GoogleFonts.jura(
 
 // Quest screen
 var screenTitle = GoogleFonts.jura(
-    fontSize: 25, color: litePurple, fontWeight: FontWeight.bold);
+    fontSize: 25, color: lightPurple, fontWeight: FontWeight.bold);
 var characterLevel = GoogleFonts.jura(
     fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold);
 var characterName = GoogleFonts.jura(
     fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold);
 var questTitle = GoogleFonts.jura(
-    fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold);
+    fontSize: 16,
+    color: lightPurple,
+    fontWeight: FontWeight.bold,
+    shadows: <Shadow>[
+      Shadow(
+        offset: Offset(1, 1),
+        blurRadius: 3.0,
+        color: Color.fromARGB(255, 0, 0, 0),
+      )
+    ]);
+var navigationButtonsLabels = GoogleFonts.jura(
+    fontSize: 13, color: Colors.white, fontWeight: FontWeight.bold);
 
 // Registration Screen
 var registrationTitle = GoogleFonts.jura(
-    fontSize: 25, color: litePurple, fontWeight: FontWeight.bold);
+    fontSize: 25, color: lightPurple, fontWeight: FontWeight.bold);
 var registrationText = GoogleFonts.jura(
     fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold);
 var userRegistrationTitle = GoogleFonts.jura(
-    fontSize: 18, color: litePurple, fontWeight: FontWeight.bold);
+    fontSize: 18, color: lightPurple, fontWeight: FontWeight.bold);
 var userRegistrationSub = GoogleFonts.jura(
     fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold);
 var registrationButton =

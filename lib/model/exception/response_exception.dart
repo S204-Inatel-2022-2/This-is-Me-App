f8e3d5c6cb21a14662078e773cfeba39533f714d
KenzoUMZ <kenzoumezawa@gmail.com>
2022-10-17 23:47:16 -0300

    register exception

--- lib/model/exception/response_exception.dart
+++ lib/model/exception/response_exception.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 class ResponseException {
-  final String message;
+  final dynamic message;
 
   ResponseException({required this.message});
 
   factory ResponseException.fromJson(Map<String, dynamic> json) {
-    return ResponseException(message: json['message']);
+    return ResponseException(message: json['message'].toString());
   }
 }
